64da0715221b41676034bc0b7fcb3ba760ee939e
Max Shi <shimax@gmail.com>
2022-04-22 00:03:34 +0000

    remove

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  * Program to implement binary search tree.
  * By: Max Shi
- * 4/4/2022
+ * 4/21/2022
  */
 
 #include <iostream>
@@ -21,24 +21,42 @@ bool isNum(char input[]);
 int charToInt(char input[]);
 void print(node* root, int count);
 void add(node* &root, int value);
-void remove();
-bool search(node* root, int value);
+void remove(node* &root, node* parent, int value);
+node* search(node* &root, int value);
+node* findSuccessorWithTwoChildren(node* &parent, node* child2);
 
 int main()
 {
   node* tree = NULL;
   while (true) {
-    cout << "Instructions:\nEnter a number or file name or SEARCH, REMOVE, PRINT, or QUIT\n";
+    cout << "Instructions: Enter a number or file name or SEARCH, REMOVE, PRINT, or QUIT\n";
 		char input[80];
 		cin.getline(input, 80, '\n');
 
     if(strcmp(input, "SEARCH") == 0)
     {
       cout << "Enter value to search: ";
+      cin.getline(input, 80, '\n');
+      if (isNum(input) == true) { //if input is a number
+        if(search(tree, charToInt(input)) != NULL)
+        {
+          cout << input << " is in tree.\n";
+        }
+        else
+        {
+          cout << input << " is not in tree.\n";
+  
+        }
+      }
     }
     else if(strcmp(input, "REMOVE") == 0)
     {
       cout << "Enter value to remove: ";
+      cin.getline(input, 80, '\n');
+      if (isNum(input) == true) //if input is a number
+      { 
+        remove(tree, tree, charToInt(input));
+      }
     }
     else if(strcmp(input, "PRINT") == 0)
     {
@@ -69,9 +87,7 @@ int main()
   			}
   		}
     }
-    
-    
-    }
+  }
 }
 
 //checks if a char array contains only numbers. Source: heap project.
@@ -114,6 +130,7 @@ int charToInt(char input[]) {
 	return num;
 }
 
+//recursively print in reverse inorder: right, node, left, and adds newlines and spaces to display tree
 void print(node* root, int count)
 {
   if(root != NULL) 
@@ -128,42 +145,176 @@ void print(node* root, int count)
   }
 }
 
+//adds a node containing value as the proper leaf node
 void add(node* &root, int value)
 {
-  if(root == NULL)
+  if(root == NULL) //if at end of tree
   {
+
+    //create node
     node* n = new node();
     n->data = value;
     n->child1 = NULL;
     n->child2 = NULL; 
     root = n;
   }
-  else if(value > root->data)
+  else if(value > root->data) //go right subtree
   {
     add(root->child2, value);
   }
-  else
+  else //go left subtree
   {
     add(root->child1, value);
   }
 }
 
-bool search(node* root, int value)
+//searches tree and returns node of specified value
+node* search(node* &root, int value)
 {
-  if(root == NULL)
+  if(root == NULL) //node of specified value is not found
   {
-    return false;
+    return NULL; 
   }
-  else if(value == root->data)
+  else if(value == root->data) 
   {
-    return true;
+    return root;
   }
-  else if(value > root->data)
+  else if(value > root->data) //go down right subtree
   {
     search(root->child2, value);
   }
-  else
+  else //go down left subtree
   {
     search(root->child1, value);
+  }
+}
+
+//recursively searches tree and removes a node of the specified value
+void remove(node* &root, node* parent, int value)
+{
+  if(root == NULL) //nothing in tree or reaches end
+  {
+    return;
+  }
+  else if(value == root->data) //finds a matching value
+  {
+    if(root->child1 == NULL && root->child2 == NULL) //node to delete is leaf
+    {
+      if (parent != root) //if there is more than one node in the tree
+      {
+        
+         //delete the node
+        delete root->child1;
+        delete root->child2;
+        delete root;
+        
+        if (parent->child1 == root) //find which child is correct
+        {
+            parent->child1 = NULL;
+        }
+        else 
+        {
+            parent->child2 = NULL;
+        }
+
+       
+      }
+      else //if there is only one node in the tree
+      {
+        //delete the node
+        delete root->child1;
+        delete root->child2;
+        delete root;
+        root = NULL;
+      }     
+    }
+    else if(root->child1 != NULL && root->child2 != NULL) //node to delete has 2 children
+    {
+      node* succParent = root;
+      node* succ = findSuccessorWithTwoChildren(succParent, root->child2); //finds successor and its parent
+      root->data = succ->data; //copies data from succesor to root
+      remove(succ, succParent, succ->data); //deletes successor
+    }
+    else if(root->child1 != NULL) //node to delete has only child 1
+    {
+      if(parent != root) //if not at top of tree
+      {
+       if (parent->child1 == root) //find which child is correct
+        {
+          parent->child1 = root->child1; //parent is linked to root's child
+          delete root; 
+        }
+        else 
+        {
+          parent->child2 = root->child1;        
+          delete root;  
+        }
+      }
+      else
+      {
+        if (parent->child1 == root) //find which child is correct
+        {
+          root = root->child1; //root becomes child
+          delete parent; //delete old root
+        }
+        else 
+        {
+          root = root->child1;        
+          delete parent;  
+        }
+      }
+    }
+    else if(root->child2 != NULL) //node to delete has only child 2
+    {
+      if(parent != root) //if not at top of tree
+      {
+        if (parent->child1 == root) //find which child is correct
+        {
+          parent->child1 = root->child2;
+          delete root;  
+        }
+        else 
+        {
+          parent->child2 = root->child2;        
+          delete root;  
+        }
+      }
+      else
+      {
+        if (parent->child1 == root) //find which child is correct
+        {
+          root = root->child1;
+          delete parent;  
+        }
+        else 
+        {
+          root = root->child1;        
+          delete parent;  
+        }
+      }
+    }
+  }
+  else if(value > root->data) //goes down right subtree
+  {
+    remove(root->child2, root, value);
+  }
+  else //goes down left subtree
+  {
+    remove(root->child1, root, value);
+  }
+  
+}
+
+//finds the inorder successor of a node with 2 children. Uses the right child of the node(child2). Additionally sets the parent to the parent of the successor.
+node* findSuccessorWithTwoChildren(node* &parent, node* child2)
+{
+  if(child2->child1 == NULL) //finds the leftmost node of the parent's right child 
+  {
+    return child2;
+  }
+  else 
+  {
+    parent = child2;
+    findSuccessorWithTwoChildren(parent, child2->child1);
   }
 }
